ed785962c2e01ca9225808c4b3a46a8fb0a2c96d
mustafa <mltm0204@gmail.com>
2022-02-05 13:54:07 +0300

    home tsx and css files are created

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import './Home.css'
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
-const Home = () => {
-  return <div></div>;
-};
-
-export default Home;
+export default function Home() {
+  return (
+    <ButtonGroup variant="contained" aria-label="outlined primary button group" className='home'>
+      <Button>CONTINENTS</Button>
+      <Button>CHART OF THE CONTINENTS</Button>
+    </ButtonGroup>
+  );
+}
